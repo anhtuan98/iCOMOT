c6e6ace423e0408731e59a51ed4060f3bfb88f5e
Le Duc Hung <d.le@dsg.tuwien.ac.at>
2015-06-05 18:50:35 +0200

    add sensor code, chiller sensor data and sensor data code

---
+++ examples/sensors/ChillerSensorData/runSensor_condenser_rule_oat.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+COL=1
+NAME=oat
+DATASET=condenser_rule
+
+echo "id,sensorName,sensorValue" > config-files/data.csv
+cat $DATASET.csv | sed 1d > $DATASET.csv.tmp
+awk -F',' '{print "'$DATASET','$NAME',"$'$COL'}' $DATASET.csv.tmp >> config-files/data.csv
+rm $DATASET.csv.tmp
+
+java -cp 'bootstrap_container-0.0.1-SNAPSHOT-jar-with-dependencies.jar:*' container.Main
